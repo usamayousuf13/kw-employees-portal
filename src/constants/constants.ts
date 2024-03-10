b4571ef0c945a59ce7b22a1b1c09ca26d7551aec
usama5 <usama@nxn.com.pk>
2024-03-10 21:24:27 +0500

    cleanup main branch

--- src/constants/constants.ts
+++
@@ -1,8 +0,0 @@
-export const LISTING = {
-    API_URL: 'https://randomuser.me/api/?results=',
-    TOTAL_USERS_COUNT: 50,
-    RESULTS_PER_PAGE: 10,
-    TABLE_HEAD: ["Name", "Gender", "Email", "Phone", ""]
-}
-export const APP_NAME = 'Kwanso Employees Portal'
-
